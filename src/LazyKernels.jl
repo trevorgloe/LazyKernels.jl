d72a2c70367aefacbc58f92aee91371f6aff2a8e
Trevor Loe <trevorgloe@gmail.com>
2026-09-17 16:29:52 -0700

    fixed mistake with two versions of mul! and example

--- src/LazyKernels.jl
+++ src/LazyKernels.jl
@@ -5,7 +5,6 @@ using LinearAlgebra
 include("KernelOperator.jl")
 include("ExecutionPlan.jl")
 using KernelAbstractions
-import LinearAlgebra: mul!
 
 struct LazyKernelMatrix{T,Ke,P<:AbstractExecutionPlan} <: AbstractMatrix{T}
     X::AbstractArray{T} # X points
@@ -33,41 +32,6 @@ function LinearAlgebra.mul!(y::AbstractVector, K::LazyKernelMatrix, v::AbstractV
         throw(ArgumentError("output vector is not on the same device as is specified by LazyKernelMatrix"))
     end
     apply!(K.Plan, K.Ker, K.X, K.Y, y, v)
-end
-# function apply!(y::AbstractVector, K::LazyKernelMatrix, v::AbstractVector)
-#     # check dimensions
-#     if (size(K, 2) != length(v)) || (size(K, 1) != length(y))
-#         throw(ArgumentError("Matrix-vector size missmatch. LazyKernelMatrix is $(size(K,1)) x $(size(K,2)), input vector is length $(length(v)), output vector is length $(length(y))"))
-#     end
-#
-#     @kernel function mulker!(y, Ker, X, Y, v)
-#         i = @index(Global)
-#         tmp_sum = zero(eltype(y))
-#         for j in 1:size(Y, 1)
-#             tmp_sum += Ker(X, Y, i, j) * v[j]
-#         end
-#         y[i] = tmp_sum
-#     end
-#
-#     # check for backend mismatch
-#     if (get_backend(K.X) != K.Plan.backend) || (get_backend(K.Y) != K.Plan.backend)
-#         throw(ArgumentError("Backends do not match. X is on $(get_backend(K.X)), Y is on $(get_backend(K.Y)), and the LazyKernelMatrix object has backend specified as $(K.Plan.backend)"))
-#     end
-#     if (get_backend(v) != K.Plan.backend)
-#         throw(ArgumentError("input vector is not on the same device as is specified by LazyKernelMatrix"))
-#     end
-#     if (get_backend(y) != K.Plan.backend)
-#         throw(ArgumentError("output vector is not on the same device as is specified by LazyKernelMatrix"))
-#     end
-#
-#     kernel! = mulker!(K.Plan.backend, K.Plan.workgroup_n)
-#     kernel!(y, K.Ker, K.X, K.Y, v; ndrange=length(y))
-#     synchronize(K.Plan.backend)
-# end
-
-function mul!(y::AbstractVector, K::LazyKernelMatrix, v::AbstractVector)
-    # just feed through to apply! method
-    apply!(y, K, v)
     return y
 end
 
